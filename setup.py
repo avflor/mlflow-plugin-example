1366455c368cd26795fbb7be4280497eaea08aa1
Corey Zumar <corey.zumar@databricks.com>
2019-10-28 23:50:45 -0700

    Setup reqs

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
-setup(name='sqlplugin',
-      version='1.0',
-      description='SQL plugin example for MLflow',
-      author='Corey Zumar',
-      author_email='corey.zumar@databricks.com',
-      packages=['sqlplugin'],
-     )
+setup(
+  name='sqlplugin',
+  version='1.0',
+  description='SQL plugin example for MLflow',
+  author='Corey Zumar',
+  author_email='corey.zumar@databricks.com',
+  packages=['sqlplugin'],
+  install_requires=[
+      'mlflow',
+  ],
+)
